372c9e9ec5757d85560b1d0f39f5c03529d4e880
Tony Lea <tonylea@nhs.net>
2018-10-03 14:28:14 +0100

    use bcrypt  to hash passwords in database

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+
 export default {
   Query: {
     getUser: (parent, { id }, { models }) =>
@@ -5,6 +7,15 @@ export default {
     allUsers: (parent, args, { models }) => models.User.findAll()
   },
   Mutation: {
-    createUser: (parent, args, { models }) => models.User.create(args)
+    registerUser: async (parent, { password, ...otherArgs }, { models }) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 12);
+        await models.User.create({ ...otherArgs, password: hashedPassword });
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    }
   }
 };
